ad9d38b14d2598a389649ec311d84ec087998b35
datapirates0101 <datapirates0101@gmail.com>
2022-04-23 12:29:32 +0530

    added some animation

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laundry/screens/login_screen.dart';
 import 'package:laundry/widgets/onBoarding.dart';
 
 class Onboarding extends StatefulWidget {
@@ -9,9 +10,36 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
-  PageController _pageController = PageController(
+  int currentPage = 0;
+  final PageController _pageController = PageController(
     initialPage: 0,
   );
+
+  AnimatedContainer getIndicator(int pageNo) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 100),
+      height: 10,
+      width: currentPage == pageNo ? 20 : 10,
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        color: currentPage == pageNo ? Colors.orange : Colors.grey,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(5),
+        ),
+      ),
+    );
+  }
+
+  void changePage() {
+    if (currentPage == 4) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: ((context) => LoginPage())));
+    } else {
+      _pageController.animateToPage(currentPage + 1,
+          duration: const Duration(milliseconds: 200), curve: Curves.linear);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,11 +57,11 @@ class _OnboardingState extends State<Onboarding> {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
+              SizedBox(
                 height: 500,
                 child: PageView(
                   controller: _pageController,
-                  children: [
+                  children: const [
                     OnboardingCard(
                       img: "onBoard1",
                       title: "Choose Clothes",
@@ -55,10 +83,42 @@ class _OnboardingState extends State<Onboarding> {
                       title: "Pay Later",
                     ),
                   ],
+                  onPageChanged: (value) => setState(() {
+                    currentPage = value;
+                  }),
                 ),
               ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(5, (index) => getIndicator(index))),
             ],
           ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: InkWell(
+              onTap: changePage,
+              splashColor: Colors.white,
+              child: Container(
+                height: 70,
+                width: 70,
+                margin: const EdgeInsets.only(bottom: 30),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                      colors: [Color(0xfff3953b), Color(0xffe57509)],
+                      stops: [0, 1],
+                      begin: Alignment.topCenter),
+                ),
+                child: const Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
